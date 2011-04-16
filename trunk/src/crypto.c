f919d92afe186e846734b48b07ce01e270d7b53d
zeminlu <zeminlu@29d62143-db55-3a83-a316-00a0c7fa84d6>
2011-04-16 18:47:45 +0000

    Arreglo bugs, modifique los enums de algoritmo y modo, arrancando los mismos ahora desde 0 en lugar de 1

--- trunk/src/crypto.c
+++ trunk/src/crypto.c
@@ -15,17 +15,17 @@ evpCipherFunc evpCipherFuncs[4][4] = {
 };
 
 long cryptWithPass(void *inData, long data_len, void *outData, t_opt crypt_mode, t_alg algorithm, t_mode mode, char *pass){
-    evpCipherFunc type = evpCipherFuncs[algorithm][mode];
-    int keySize = algorithm == AES192 ? 20 : (algorithm == AES256 ? 24 : 16);
+    evpCipherFunc type = evpCipherFuncs[mode][algorithm];
+    int keySize = (algorithm == AES256) ? 32 : (algorithm == AES192 ? 24 : 16);
     int derivedKeySize, cryptSize;
     unsigned char *key = malloc(keySize);
     unsigned char *iv = malloc(keySize);    
     
     if ((derivedKeySize =  EVP_BytesToKey(type(), EVP_md5(), NULL, (unsigned char *)pass, strlen(pass), 1, key, iv)) != keySize){
-        printf("keySize & derivedKeySize differ");
+        printf("keySize & derivedKeySize differ, keySize = %d, derivedKeySize = %d\n", keySize, derivedKeySize);
         return -1;
     }
-    
+        
     cryptSize = cryptMe(inData, data_len, outData, type, crypt_mode, key, iv);
     
     varFree(2, key, iv);
@@ -35,7 +35,7 @@ long cryptWithPass(void *inData, long data_len, void *outData, t_opt crypt_mode,
 
 long cryptWithKey(void *inData, long data_len, void *outData, t_opt crypt_mode, t_alg algorithm, t_mode mode, char *key, char *iv){
     int cryptSize;
-    evpCipherFunc type = evpCipherFuncs[algorithm][mode];
+    evpCipherFunc type = evpCipherFuncs[mode][algorithm];
     
     cryptSize = cryptMe(inData, data_len, outData, type, crypt_mode, (unsigned char *)key, (unsigned char *)iv);
     
@@ -50,27 +50,27 @@ int cryptMe(void *inData, long data_len, void *outData, evpCipherFunc type, t_op
     EVP_CIPHER_CTX_init(&ctx); 
         
     if (EVP_CipherInit_ex(&ctx, type(), NULL, key, iv, crypt_mode) == 0){
-        printf("Error en cipherInit");
+        printf("Error en cipherInit\n");
         return -1;
     }
     
     if (EVP_CipherUpdate(&ctx, outData, &outl, inData, data_len) == 0){
-        printf("Error en cipherUpdate");
+        printf("Error en cipherUpdate\n");
         return -1;
     }
     
     if (EVP_CipherFinal_ex(&ctx, (unsigned char *) outData + outl, &templ) == 0){
-        printf("Error en cipherFinal");
+        printf("Error en cipherFinal\n");
         return -1;
     }
     
     if (EVP_CIPHER_CTX_cleanup(&ctx) == 0){
-        printf("Error en cipherCleanup");
+        printf("Error en cipherCleanup\n");
         return -1;
     }
-    
+        
     if (outl + templ != data_len){
-        printf("El tamaño de los datos encriptados es distinta al de los datos desencriptados, outl = %d, templ = %d", outl, templ);
+        printf("El tamaño de los datos encriptados es distinto al de los datos desencriptados, outl = %d, templ = %d\n", outl, templ);
         return -1;
     }
     
